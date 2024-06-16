4b2992b2b9d1d5d3900aebf81f9cd7558fb9413f
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-06-16 13:37:03 +0300

    Update preparation.sh

--- ext/preparation.sh
+++ ext/preparation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "Устанавливаем nodejs и необходимые приложения"
+echo -e "Устанавливаем nodejs и всё необходимое"
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg unzip
 sudo mkdir -p /etc/apt/keyrings
@@ -9,11 +9,9 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-echo -e "Устанавливаем используемые в скриптах npm пакеты"
 sudo apt-get update
 npm install axios js-base64 fs path
 
-echo -e "Создаём основную директорию и загружаем каталог с хорошим кодом"
 sudo mkdir /root/github_update && cd /root/github_update
 curl -fsSL https://github.com/AndriiKok/Activity/raw/main/ext/files.zip > "/root/github_update/files.zip"
 unzip -q -d /root/github_update files.zip && rm -rf /root/github_update/files.zip
