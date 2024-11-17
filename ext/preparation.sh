3b65cb060a0c74b046e3455ce2c9dfe591d9d194
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-17 10:43:21 +0200

    Update preparation.sh

--- ext/preparation.sh
+++ ext/preparation.sh
@@ -2,12 +2,10 @@
 
 echo -e "Устанавливаем nodejs и всё необходимое"
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg unzip
-sudo mkdir -p /etc/apt/keyrings
-rm -f /etc/apt/keyrings/nodesource.gpg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+source ~/.bashrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install v22.11.0
 
 sudo apt-get update
 npm install axios js-base64 fs path
